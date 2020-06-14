7c349475658dfbf49b8ce46972510a40d3edfca2
sourav <gurung.sg43@gmail.com>
2020-06-14 22:30:54 +0545

    reponsive left

--- script.js
+++ script.js
@@ -4,10 +4,42 @@ $(function(){
         var currentImage = $(this).attr('data-image');
         // console.log(currentImage);
         $(this).parent().parent().parent().find('img').attr("src", currentImage);
-        // $(".category-section img").css("opacity", "1");
+        $(".category-section img").css("opacity", "1");
     });
 
-    // $(".category-section ul li a").mouseout(function(){
-    //     $(".category-section img").css("opacity", "0.5");
-    // });
+   $(".category-section ul li a").mouseout(function(){
+        $(".category-section img").css("opacity", "1");
+    });
 });
+
+// search bar expanding
+
+$(document).ready(function(){
+    $('.search-icon').click(function(){
+        $('.search-bar').toggleClass('active');
+    })
+})
+
+// hamburger animation
+const menuBtn = document.querySelector('.navigation-bar');
+let menuOpen = false;
+menuBtn.addEventListener('click', () =>{
+    if(!menuOpen){
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } 
+    else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
+
+// mega menu navigation
+
+$(document).ready(function(){
+    $('.hamburger-menu').click(function(){
+        $('.mega-menu').toggleClass('active');
+    });
+});
+
+
